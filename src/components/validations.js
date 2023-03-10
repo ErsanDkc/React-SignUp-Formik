c7274d5584978c92eef10fcf8f83964f97620b52
ErsanDikici <edikici55@gmail.com>
2023-03-10 22:23:21 +0300

    update

--- src/components/validations.js
+++ src/components/validations.js
@@ -4,10 +4,7 @@ const validations = object({
     firstName: string().required("Please Enter Your FirsName"),
     lastName: string().required("Please Enter Your LastName"),
     email: string().email("Email is invalid!").required("Please Enter Your email"),
-    password: string().max(12,"Password must be no more then 12").required("Please Enter Your Password").matches(
-      `/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/`,
-      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
-    ),
+    password: string().max(12,"Password must be no more then 12").required("Please Enter Your Password").min(8),
     passwordConfirm : string().required("Please Enter Your Confirm Password").oneOf([ref("password")],"Passwords Must Match")
     
 
